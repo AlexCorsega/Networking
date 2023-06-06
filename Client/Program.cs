ba0a584a9048a66a26c1c0a8f9f90a5300bb91eb
Alex Corsega Jr <111210922+AlexCorsega@users.noreply.github.com>
2023-06-06 20:08:13 +0800

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Client
+{
+    class Program
+    {
+        static TcpClient _client;
+        static Socket _socket;
+        static void Main(string[] args)
+        {
+            _client = new TcpClient("192.168.1.4", 1234);
+            //_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //_socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //_socket.Connect(IPAddress.Loopback, 1234);
+            //_client.Client = _socket;
+            //Console.WriteLine($"My IPAddress {_client.Client.LocalEndPoint.ToString()}");
+            //_socket.Send(ASCIIEncoding.ASCII.GetBytes(message));
+            NetworkStream ns = _client.GetStream();
+            while (true)
+            {
+                Console.Write("Enter Message: ");
+                string message = Console.ReadLine();
+                byte[] byteTosend = ASCIIEncoding.ASCII.GetBytes(message);
+                ns.Write(byteTosend, 0, byteTosend.Length);
+                byte[] bytesToRead = new byte[_client.ReceiveBufferSize];
+                int bytesRead = ns.Read(bytesToRead, 0, _client.ReceiveBufferSize);
+                Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                //Console.Clear();
+                //_socket.Close();
+            }
+            _client.Close();
+
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net.NetworkInformation;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Networking
+{
+    class Program
+    {
+        static TcpListener _tcpListener;
+        static WebClient _webClient;
+        static void Main(string[] args)
+        {
+            //foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            //{
+            //    Console.WriteLine("Name: " + netInterface.Name);
+            //    Console.WriteLine("Description: " + netInterface.Description);
+            //    Console.WriteLine("Addresses: ");
+            //    IPInterfaceProperties ipProps = netInterface.GetIPProperties();
+            //    foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
+            //    {
+            //        Console.WriteLine(" " + addr.Address.ToString());
+            //    }
+            //    Console.WriteLine("");
+            //}
+            _webClient = new WebClient();
+            string downloadString = _webClient.DownloadString("https://ifconfig.me/all.json");
+            string ip = downloadString.Substring(16, 14);
+            Console.WriteLine(ip);
+            TcpListener _tcpListener = new TcpListener(IPAddress.Parse("192.168.1.4"), 1234);
+            Console.WriteLine(_tcpListener.Server.RemoteEndPoint);
+            Console.WriteLine(_tcpListener.Server.LocalEndPoint);
+            _tcpListener.Start(5);
+            Console.WriteLine("Server Started...");
+            Console.WriteLine("Waiting For Connection...");
+            while (true)
+            {
+                TcpClient client = _tcpListener.AcceptTcpClient();
+                Task.Factory.StartNew(SetupServer, client);
+            }
+            Console.ReadKey();
+        }
+        private static void SetupServer(object client)
+        {
+            try
+            {
+                Console.WriteLine("Client IPAddress : " + (client as TcpClient).Client.RemoteEndPoint);
+                while (true)
+                {
+                    var clients = (TcpClient)client;
+                    byte[] bytes = new byte[clients.ReceiveBufferSize];
+                    NetworkStream ns = clients.GetStream();
+                    ns.Read(bytes, 0, bytes.Length);
+                    Console.Clear();
+                    Console.WriteLine(ASCIIEncoding.ASCII.GetString(bytes));
+                    ns.Write(ASCIIEncoding.ASCII.GetBytes("Hi from server"), 0, 14);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Thread.CurrentThread.Abort();
+            }
+
+        }
+        private static void BeginAcceptCallback(IAsyncResult asyncResult)
+        {
+        }
+    }
+}
